5f74b6bd92510b19e6e74c56000b3504c9c5cb27
MayaSker2 <mayasker8@gmail.com>
2024-03-14 12:43:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var navbarToggle = document.getElementById('navbarToggle');
+var navbarMenu = document.getElementById('navbarMenu');
+
+navbarToggle.addEventListener('click', function() {
+  navbarMenu.classList.toggle('active');
+});
+document.write("bkjbkj")
